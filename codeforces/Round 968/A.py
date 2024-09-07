5e16e0d06756f8294d54b2ec05886c729159f251
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2024-09-08 03:25:08 +0600

    Create A.py

---
+++ codeforces/Round 968/A.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        input()
+        s = input()
+        if s[0] == s[-1]:
+            print('no')
+        else:
+            print('yes')

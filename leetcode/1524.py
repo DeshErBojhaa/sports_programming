168ef154cf72a837a12a57191e68c6fd53dd5c6f
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2025-02-25 19:29:32 +0600

    Create 1524.py

---
+++ leetcode/1524.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        positions_where_sum_is_odd = 0
+        positions_where_sum_is_even = 1 # initially sum is 0
+        
+        tot_sum, ans = 0, 0
+        
+        for v in arr:
+            tot_sum += v
+            
+            if tot_sum%2:
+                positions_where_sum_is_odd += 1
+                ans += positions_where_sum_is_even
+            else:
+                positions_where_sum_is_even += 1
+                ans += positions_where_sum_is_odd
+                
+        return ans % (10**9+7)
+        
+        

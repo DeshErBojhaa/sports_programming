df59659ae6034e7a06dd70250edddd1b2f292b06
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2025-11-15 15:10:32 +0600

    lalal
    Signed-off-by: Tamjid Ahmed <mail.to.tamjid@gmail.com>

--- leetcode/567 Permutation in string.py
+++ leetcode/567 Permutation in string.py
@@ -1,16 +1,23 @@
+# 567. Permutation in String
+from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        c, cc = Counter(s1), Counter()
-        start = 0
-        for i, ch in enumerate(s2):
-            if ch not in c:
-                cc.clear()
-                start = i + 1
-                continue
-            cc[ch] += 1
-            if cc == c:
-                return True
-            while cc[ch] > c[ch]:
-                cc[s2[start]] -= 1
-                start += 1
+        needle, haystack = Counter(s1), Counter()
+        
+        start, end = 0, 0
+        
+        while end < len(s2):
+            if s2[end] not in needle:
+                haystack.clear()
+                start = end + 1
+            else:
+                haystack[s2[end]] += 1
+                if needle == haystack:
+                    return True
+                while haystack[s2[end]] > needle[s2[end]]:
+                    haystack[s2[start]] -= 1
+                    start += 1
+    
+            end += 1
+        
         return False
